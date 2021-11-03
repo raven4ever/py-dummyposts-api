58faf7a092f2b6fd1a4f19c622a21f19e5c339ea
raven4ever <wrtv23@gmail.com>
2021-11-03 12:55:24 +0200

    prefix & tags

--- app/routers/post.py
+++ app/routers/post.py
@@ -6,16 +6,16 @@ from sqlalchemy.orm import Session
 from .. import models, schemas
 from ..database import get_db
 
-router = APIRouter()
+router = APIRouter(prefix='/posts', tags=['Posts'])
 
 
-@router.get('/posts', response_model=List[schemas.PostResponse])
+@router.get('/', response_model=List[schemas.PostResponse])
 def get_posts(db: Session = Depends(get_db)):
     posts = db.query(models.Post).all()
     return posts
 
 
-@router.post('/posts', status_code=status.HTTP_201_CREATED, response_model=schemas.PostResponse)
+@router.post('/', status_code=status.HTTP_201_CREATED, response_model=schemas.PostResponse)
 def create_post(new_post: schemas.PostCreate, db: Session = Depends(get_db)):
     saved_post = models.Post(**new_post.dict())
     db.add(saved_post)
@@ -25,7 +25,7 @@ def create_post(new_post: schemas.PostCreate, db: Session = Depends(get_db)):
     return saved_post
 
 
-@router.get('/posts/{id}', response_model=schemas.PostResponse)
+@router.get('/{id}', response_model=schemas.PostResponse)
 def get_post(id: int, db: Session = Depends(get_db)):
     post = db.query(models.Post).filter(models.Post.id == id).first()
 
@@ -36,7 +36,7 @@ def get_post(id: int, db: Session = Depends(get_db)):
     return post
 
 
-@router.delete('/posts/{id}')
+@router.delete('/{id}')
 def delete_post(id: int, db: Session = Depends(get_db)):
     delete_query = db.query(models.Post).filter(models.Post.id == id)
 
@@ -50,7 +50,7 @@ def delete_post(id: int, db: Session = Depends(get_db)):
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 
-@router.put('/posts/{id}', response_model=schemas.PostResponse)
+@router.put('/{id}', response_model=schemas.PostResponse)
 def update_post(id: int, post: schemas.PostCreate, db: Session = Depends(get_db)):
     update_query = db.query(models.Post).filter(models.Post.id == id)
     updated_post = update_query.first()
--- app/routers/user.py
+++ app/routers/user.py
@@ -4,12 +4,12 @@ from sqlalchemy.orm import Session
 from .. import models, schemas, utils
 from ..database import get_db
 
-router = APIRouter()
+router = APIRouter(prefix='/users', tags=['Users'])
 
 
-@router.post('/users', status_code=status.HTTP_201_CREATED, response_model=schemas.UserOut)
+@router.post('/', status_code=status.HTTP_201_CREATED, response_model=schemas.UserOut)
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
-    user.password = utils.hash(user.password)
+    user.password = utils.hash_string(user.password)
 
     saved_user = models.User(**user.dict())
     db.add(saved_user)
@@ -19,7 +19,7 @@ def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     return saved_user
 
 
-@router.get("/users/{id}", response_model=schemas.UserOut)
+@router.get('/{id}', response_model=schemas.UserOut)
 def get_user(id: int, db: Session = Depends(get_db)):
     user = db.query(models.User).filter(models.User.id == id).first()
 

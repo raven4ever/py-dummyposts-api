8cc80d8fba26433e2473e2d2de0c7838704c0ca8
raven4ever <wrtv23@gmail.com>
2021-11-03 12:40:32 +0200

    added users

--- app/main.py
+++ app/main.py
@@ -1,10 +1,10 @@
 from typing import List
 from fastapi import FastAPI, Response, status, HTTPException, Depends
-
 from sqlalchemy.orm import Session
 
 from . import models
 from . import schemas
+from . import utils
 from .database import engine, get_db
 
 
@@ -74,3 +74,26 @@ def update_post(id: int, post: schemas.PostCreate, db: Session = Depends(get_db)
     db.commit()
 
     return update_query.first()
+
+
+@app.post('/users', status_code=status.HTTP_201_CREATED, response_model=schemas.UserOut)
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    user.password = utils.hash(user.password)
+
+    saved_user = models.User(**user.dict())
+    db.add(saved_user)
+    db.commit()
+    db.refresh(saved_user)
+
+    return saved_user
+
+
+@app.get("/users/{id}", response_model=schemas.UserOut)
+def get_user(id: int, db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id == id).first()
+
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"user with id {id} was not found!")
+
+    return user
--- app/schemas.py
+++ app/schemas.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
 from datetime import datetime
 
 
@@ -13,6 +13,20 @@ class PostCreate(PostBase):
 
 
 class PostResponse(PostBase):
+    created_at: datetime
+
+    class Config:
+        orm_mode = True
+
+
+class UserCreate(BaseModel):
+    email: EmailStr
+    password: str
+
+
+class UserOut(BaseModel):
+    id: int
+    email: EmailStr
     created_at: datetime
 
     class Config:

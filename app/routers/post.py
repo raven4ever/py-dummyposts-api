d9a66510947bbda19389d65a349a113b438f6c3c
raven4ever <wrtv23@gmail.com>
2021-11-04 12:09:19 +0200

    url params

--- app/routers/post.py
+++ app/routers/post.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Optional
 
 from fastapi import Response, status, HTTPException, APIRouter, Depends
 from sqlalchemy.orm import Session
@@ -10,8 +10,10 @@ router = APIRouter(prefix='/posts', tags=['Posts'])
 
 
 @router.get('/', response_model=List[schemas.PostResponse])
-def get_posts(db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
-    posts = db.query(models.Post).all()
+def get_posts(db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user),
+              limit: int = 10, skip: int = 0, search: Optional[str] = ''):
+    posts = db.query(models.Post).filter(
+        models.Post.title.contains(search)).limit(limit).offset(skip).all()
     return posts
 
 
